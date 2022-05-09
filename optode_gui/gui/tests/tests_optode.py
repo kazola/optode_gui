c29f68ba7bbf4f2d7d329ba5a38c4d9c9647ece3
kaz <kaz@pop-os.localdomain>
2022-05-09 14:28:58 -0400

    optode

--- optode_gui/gui/tests/tests_optode.py
+++ optode_gui/gui/tests/tests_optode.py
@@ -50,7 +50,7 @@ def test_btn_display_1_out(ser) -> tuple:
     return 1, ''
 
 
-def test_display_1_in(ser) -> tuple:
+def test_adc_display_1_in(ser) -> tuple:
     ser.write('6'.encode())
     a = ser.readall()
     if not a:
@@ -58,13 +58,17 @@ def test_display_1_in(ser) -> tuple:
         return 1, ''
 
     # a: b'768'
+    print('adc display #1 value =', a)
     if int(a.decode()) < 100:
-        return 0, 'display off'
-    return 0, 'display on'
+        return 0, 'display #1 is OFF'
+
+    # give time thing to boot
+    time.sleep(5)
+    return 0, 'display #1 is ON'
 
 
 def test_led_strip_arduino(ser) -> tuple:
-    ser.write('7'.encode())
+    ser.write('4'.encode())
     a = ser.readall()
     print(a)
     if a == b'leds':
@@ -72,17 +76,29 @@ def test_led_strip_arduino(ser) -> tuple:
     return 1, ''
 
 
-def test_wifi_1(ser) -> tuple:
+def test_btn_wifi_1_out(ser) -> tuple:
+    ser.write('7'.encode())
+    a = bytes()
+    n = int(2 / SERIAL_BYTE_TIMEOUT)
+    for i in range(n):
+        a += ser.read()
+
+    if a == b'0':
+        return 0, ''
+    return 1, ''
+
+
+def test_adc_wifi_1(ser) -> tuple:
     ser.write('8'.encode())
     a = ser.readall()
     if not a:
         return 1, ''
 
     # a: b'768'
-    print(a)
-    if int(a.decode()) < 100:
-        return 0, 'wifi_1 off'
-    return 0, 'wifi_1 on'
+    print('adc wifi #1 value =', a)
+    if int(a.decode()) < 400:
+        return 0, 'wifi #1 is OFF'
+    return 0, 'wifi #1 is ON'
 
 
 def test_motor_adc(ser) -> tuple:
--- optode_gui/main_utils.py
+++ optode_gui/main_utils.py
@@ -2,8 +2,9 @@ import time
 from optode_gui.gui.gui_utils import gui_busy_get, gui_trace_clear, gui_trace, gui_trace_rv, \
     gui_busy_free
 from optode_gui.gui.tests.tests_optode import test_serial_arduino, test_12v_arduino, test_5v_arduino, \
-     test_btn_display_1_out, \
-    test_display_1_in, test_led_strip_arduino, test_wifi_1, test_motor_adc, test_motor_movement, test_motor_switches
+    test_btn_display_1_out, \
+    test_adc_display_1_in, test_led_strip_arduino, test_adc_wifi_1, test_motor_adc, test_motor_movement, \
+    test_motor_switches, test_btn_wifi_1_out
 
 
 def btn_tests(g, ser):
@@ -37,35 +38,42 @@ def btn_tests(g, ser):
     # gt_rv(g, rv, 'test_vcc5v')
     # gt(g, '\n')
 
-    rv = test_led_strip_arduino(ser)
-    gt_rv(g, rv, 'test_led_strip')
+    #rv = test_led_strip_arduino(ser)
+    #gt_rv(g, rv, 'test_led_strip')
+    #gt(g, '\n')
+
+    # -----------
+    # display #1
+    # -----------
+    gt(g, 'please look at iris #1 display')
+    rv = test_btn_display_1_out(ser)
+    gt_rv(g, rv, 'test_btn_display_out_1')
+    gt(g, '\n')
+    rv_dis = rv = test_adc_display_1_in(ser)
+    gt_rv(g, rv, 'test_adc_display_1')
     gt(g, '\n')
 
-    # rv = test_btn_display_1_out(ser)
-    # gt_rv(g, rv, 'test_btn_display_out_1')
-    # gt(g, '\n')
-    #
-    # rv_dis = rv = test_display_1_in(ser)
-    # gt_rv(g, rv, 'test_vcc3v_display')
-    # gt(g, '\n')
+    # ---------
+    # wifi # 1
+    # ---------
+    gt(g, 'activating wi-fi #1')
+    rv = test_btn_wifi_1_out(ser)
+    gt_rv(g, rv, 'test_btn_wifi_1_out')
+    gt(g, '\n')
+    rv = test_adc_wifi_1(ser)
+    gt_rv(g, rv, 'test_adc_wifi_1')
+    gt(g, '\n')
 
-    # if rv_dis[1] == 'display on':
-    #     for i in range(10):
-    #         gt(g, 'will check wi-fi in {}'.format(10 - i))
-    #         time.sleep(1)
-    #     rv = test_wifi_1(ser)
-    #     gt_rv(g, rv, 'test_vcc_wifi_1')
-    #     gt(g, '\n')
 
     #rv_adc_mot = rv = test_motor_adc(ser)
     #gt_rv(g, rv, 'test_motor_adc')
     #gt(g, '\n')
 
-    gt(g, '[ .... ] look / hear motor moving')
-    time.sleep(.1)
-    rv = test_motor_movement(ser)
-    gt_rv(g, rv, 'motor_test_run')
-    gt(g, '\n')
+    #gt(g, '[ .... ] look / hear motor moving')
+    #time.sleep(.1)
+    #rv = test_motor_movement(ser)
+    #gt_rv(g, rv, 'motor_test_run')
+    #gt(g, '\n')
 
     #rv = test_motor_switches(ser)
     #gt_rv(g, rv, 'test_motor_switches')
